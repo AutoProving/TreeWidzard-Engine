8db47218920f73730841d4959208c10c10c253bd
Mateus <mateus.oliveira@gmail.com>
2021-03-25 18:45:58 +0100

    parameters to random search

--- Scripts/plugins.sh
+++ Scripts/plugins.sh
@@ -1,20 +1,19 @@
   #!/bin/bash
   p=$PWD;
-  rm -r $PWD/../DynamicPlugins/*
-  for eachfile in $(find $PWD/../DynamicCores -type f -name "VertexCon*.cpp")
+#  rm -r $PWD/../DynamicPlugins/*
+#  for eachfile in $(find $PWD/../DynamicCores -type f -name "*.cpp")
+#  do
+#      name=$(basename "$eachfile" .cpp)
+#      echo $name
+#      echo $eachfile
+#      g++ -fPIC -shared $p/../Kernel/WitnessSet.cpp $p/../Kernel/Witness.cpp $p/../Kernel/DynamicCore.cpp $eachfile -o $p/../DynamicPlugins/$name.so
+#  done
+#  rm -r $PWD/../SearchPlugins/*
+  for eachfile in $(find $PWD/../Search -type f -name "Random*Connected.cpp")
   do
       name=$(basename "$eachfile" .cpp)
       echo $name
       echo $eachfile
-      g++ -fPIC -shared $p/../Kernel/WitnessSet.cpp $p/../Kernel/Witness.cpp $p/../Kernel/DynamicCore.cpp $eachfile -o $p/../DynamicPlugins/$name.so
+      echo $p
+      g++ -fPIC -shared $p/../Kernel/DynamicKernel.cpp  $p/../Kernel/DynamicCore.cpp  $p/../Kernel/Conjecture.cpp  $p/../Kernel/SearchStrategy.cpp  $eachfile  -o  $p/../SearchPlugins/$name.so
   done
-#   rm -r $PWD/../SearchPlugins/*
-#   for eachfile in $(find $PWD/../Search -type f -name "*.cpp")
-#   do
-#       name=$(basename "$eachfile" .cpp)
-#       echo $name
-#       echo $eachfile
-#       echo $p
-#       g++ -fPIC -shared $p/../Kernel/DynamicKernel.cpp  $p/../Kernel/DynamicCore.cpp  $p/../Kernel/Conjecture.cpp  $p/../Kernel/SearchStrategy.cpp  $eachfile  -o  $p/../SearchPlugins/$name.so
-#
-#   done
