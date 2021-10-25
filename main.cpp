cbbb7c263e671d4a61c192db0b9e49239824719f
fva061@uib.no <farhad.vadiee@uib.no>
2021-10-25 15:28:15 +0200

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,11 +6,17 @@
 #include <chrono>
 #include <sys/time.h>
 #include <ctime>
+#include <filesystem>
+#include <unistd.h>
+
 
 using namespace std;
 extern FILE *command_in;
+
+
 using namespace std;
 
+namespace fs = std::filesystem;
 
 using std::cout; using std::endl;
 using std::chrono::duration_cast;
@@ -19,6 +25,7 @@ using std::chrono::seconds;
 using std::chrono::system_clock;
 
 int main(int argc, char *arg[]) {
+    // Reading the input and put it in string _all_arg
     std::string _all_arg;
     for (int i = 1 ; i < argc; i++) {
         _all_arg += arg[i];
@@ -28,32 +35,58 @@ int main(int argc, char *arg[]) {
             _all_arg +="\n;";
         }
     }
-    //cout<<_all_arg;
-    // Create and open a text file
-    int iSecret;
 
-    /* initialize random seed: */
-    srand (time(NULL));
+    // Generating the temp file pFile
+    char buffer [256];
+    FILE * pFile;
+    pFile = tmpfile(); // c++ function for generating a temporary file
 
-    /* generate secret number between 1 and 100: */
-    iSecret = rand() % 100 + 1;
-    auto millisec_since_epoch = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
-    string s = "auxfiles/"+to_string(millisec_since_epoch)+"_"+to_string(iSecret)+".txt";
-    cout<<s<<endl;
-    ofstream MyFile(s);
-    // Write to the file
-    MyFile << _all_arg;
-    // Close the file
-    MyFile.close();
-    command_in = fopen(s.c_str(), "r");
-    if (!command_in) {
-        std::perror("File opening failed");
-        return EXIT_FAILURE;
+    fputs (_all_arg.c_str(),pFile); // Write _all_arg in pFile
+    rewind(pFile); // Sets the position indicator associated with stream to the beginning of the file.
+    command_in = pFile; // Set command_in equals to pFile
+
+
+    /* Test to print the content of command_in
+    while (!feof(command_in)) {
+        if (fgets (buffer,256,command_in) == NULL) break;
+        fputs (buffer,stdout);
     }
+    rewind(pFile);
+    */
+
+/* Test for checking the directory of the temporary file
+    int MAXSIZE = 0xFFF;
+    char proclnk[0xFFF];
+    char filename[0xFFF];
+    FILE *fp;
+    int fno;
+    ssize_t r;
+    fp = pFile;
+
+    if (fp != NULL)
+    {
+        fno = fileno(fp);
+        sprintf(proclnk, "/proc/self/fd/%d", fno);
+        r = readlink(proclnk, filename, MAXSIZE);
+        if (r < 0)
+        {
+            printf("failed to readlink\n");
+            exit(1);
+        }
+        filename[r] = '\0';
+        printf("fp -> fno -> filename: %p -> %d -> %s\n",
+               fp, fno, filename);
+    }
+*/
+
+    // Calling the input parser
     int result_arg=10;
     string width_type;
     int width_value;
     result_arg = command_parse(result_arg,width_type,width_value);
 
-	return 0;
+    fclose(command_in); // closing command_in
+
+    return 0;
+
 }
