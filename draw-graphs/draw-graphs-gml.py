18eedfe75dafc0267e2a0cb2c8966da27553d3d1
fva061@uib.no <farhad.vadiee@uib.no>
2022-01-10 17:29:10 +0100

    update rep

--- draw-graphs/draw-graphs-gml.py
+++ draw-graphs/draw-graphs-gml.py
@@ -7,6 +7,8 @@ import pathlib
 from networkx.drawing.nx_pydot import graphviz_layout
 import os
 import sys
+import nxviz as nv 
+from nxviz import plots
 
 print(sys.argv[1])
 
@@ -17,16 +19,20 @@ G = nx.read_gml(s, label='id')
 subax1 = plt.subplot()
 #labels = nx.get_node_attributes(G, 'l')
 options = {
-    "font_size": 7,
-    "node_size": 120,
+    "font_size": 12,
+    "node_size": 400,
     "node_color": "white",
     "edgecolors": "black",
     "font_color": "green",
     "font_weight": "bold",
-    "linewidths": .8,
+    "linewidths": .9,
     "width": .8,
 }
-pos = graphviz_layout(G, prog="dot")
-nx.draw(G, pos, with_labels = True, **options)
+#pos = graphviz_layout(G, prog="dot")
+#nv.circos(G)
+
+#nx.draw_kamada_kawai(G, with_labels = True, **options)
+# nx.draw(G, pos, with_labels = True, **options)
+
 
 plt.show()
