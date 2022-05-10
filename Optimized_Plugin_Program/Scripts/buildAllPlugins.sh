c7e5be4b780c8da81c78f57a4bbf4659f10dab76
fva061@uib.no <farhad.vadiee@uib.no>
2022-05-10 16:57:42 +0200

    add hamiltonian property and changed script file

--- Optimized_Plugin_Program/Scripts/buildAllPlugins.sh
+++ Optimized_Plugin_Program/Scripts/buildAllPlugins.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 p=$PWD
-rm -r $PWD/../DynamicPlugins/*
+rm -r $PWD/../DynamicPlugins/*.so
 for eachfile in $(find $PWD/../DynamicCores -type f -name "*.cpp"); do
   name=$(basename "$eachfile" .cpp)
   echo $name
   echo $eachfile
   g++ -fPIC -shared $p/../Kernel/WitnessSet.cpp $p/../Kernel/Witness.cpp $p/../Kernel/DynamicCore.cpp $eachfile -o $p/../DynamicPlugins/$name.so
 done
-rm -r $PWD/../SearchPlugins/*
+rm -r $PWD/../SearchPlugins/*.so
 for eachfile in $(find $PWD/../Search -type f -name "*.cpp"); do
   name=$(basename "$eachfile" .cpp)
   echo $name
