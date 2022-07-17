243879f3d6d0b65cfaef33b9f6e710749a754592
fva061@uib.no <farhad.vadiee@uib.no>
2022-07-17 15:45:21 +0200

    compile scripts are updated

--- Optimized_Plugin_Program/Scripts/buildAllSearches.sh
+++ Optimized_Plugin_Program/Scripts/buildAllSearches.sh
@@ -2,15 +2,15 @@
 p=$PWD
 rm -r $PWD/../SearchPlugins/*.so
 
-for eachfile in $(find $PWD/../Search -type f -name "*.cpp"); do
-#for eachfile in $(find $PWD/../Search/BFS/ParallelBreadthFirstSearch.cpp -type f -name "*.cpp"); do
-  name=$(basename "$eachfile" .cpp)
-  echo $name
-  echo $eachfile
-  echo $p
-  g++ -pthread -fPIC -shared $p/../Kernel/DynamicKernel.cpp $p/../Kernel/DynamicCore.cpp $p/../Conjecture/Conjecture.cpp $p/../Kernel/SearchStrategy.cpp $eachfile -o $p/../SearchPlugins/$name.so
-  #g++ -pthread -fsanitize=thread -g3 -fPIC -shared $p/../Kernel/DynamicKernel.cpp $p/../Kernel/DynamicCore.cpp $p/../Conjecture/Conjecture.cpp $p/../Kernel/SearchStrategy.cpp $eachfile -o $p/../SearchPlugins/$name.so &
-done
+#for eachfile in $(find $PWD/../Search -type f -name "*.cpp"); do
+##for eachfile in $(find $PWD/../Search/BFS/ParallelBreadthFirstSearch.cpp -type f -name "*.cpp"); do
+#  name=$(basename "$eachfile" .cpp)
+#  echo $name
+#  echo $eachfile
+#  echo $p
+#  g++ -pthread -fPIC -shared $p/../Kernel/DynamicKernel.cpp $p/../Kernel/DynamicCore.cpp $p/../Conjecture/Conjecture.cpp $p/../Kernel/SearchStrategy.cpp $eachfile -o $p/../SearchPlugins/$name.so
+#  #g++ -pthread -fsanitize=thread -g3 -fPIC -shared $p/../Kernel/DynamicKernel.cpp $p/../Kernel/DynamicCore.cpp $p/../Conjecture/Conjecture.cpp $p/../Kernel/SearchStrategy.cpp $eachfile -o $p/../SearchPlugins/$name.so &
+#done
 
 for eachfile in $(find $PWD/../../../treewidzard-Searches-Temp -type f -name "*.cpp"); do
 #for eachfile in $(find $PWD/../Search/BFS/ParallelBreadthFirstSearch.cpp -type f -name "*.cpp"); do
@@ -18,7 +18,7 @@ for eachfile in $(find $PWD/../../../treewidzard-Searches-Temp -type f -name "*.
   echo $name
   echo $eachfile
   echo $p
-  g++ -pthread -fPIC -shared $p/../Kernel/DynamicKernel.cpp $p/../Kernel/DynamicCore.cpp $p/../Conjecture/Conjecture.cpp $p/../Kernel/SearchStrategy.cpp $eachfile -o $p/../SearchPlugins/$name.so
+  g++ -pthread -fPIC -shared $p/../Kernel/DynamicKernel.cpp $p/../Kernel/DynamicCore.cpp $p/../Conjecture/Conjecture.cpp $p/../Kernel/SearchStrategy.cpp $eachfile -I $p/../Kernel/ -I $p/../TreeAutomaton/ -I $p/../parallel_hashmap/ -I $p/../LockedSet  -o $p/../SearchPlugins/$name.so
   #g++ -pthread -fsanitize=thread -g3 -fPIC -shared $p/../Kernel/DynamicKernel.cpp $p/../Kernel/DynamicCore.cpp $p/../Conjecture/Conjecture.cpp $p/../Kernel/SearchStrategy.cpp $eachfile -o $p/../SearchPlugins/$name.so &
 done
 
